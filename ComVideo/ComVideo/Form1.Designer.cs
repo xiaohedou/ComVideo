e97e028b8d4f3130b1ad5663cc7af1c832eb08ae
小禾斗 <30337325+xiaohedou@users.noreply.github.com>
2018-12-05 11:09:41 +0800

    MP4合成器V1.0
    C#实现的MP4合成器，使用场景：合成多个MP4文件、给视频添加片头片尾

---
+++ ComVideo/ComVideo/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ComVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "合成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 193);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "选择视频保存路径：";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(365, 123);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "…";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(121, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(222, 21);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "设置片尾：";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(365, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "…";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(121, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 21);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "选择要合成的视频：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "…";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "设置片头：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "…";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(12, 203);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(438, 272);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件名";
+            this.columnHeader1.Width = 600;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "所有文件|*.*|视频|*.mp4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 481);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MP4视频合成器 V1.2【作者：小科】";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ComVideo/ComVideo/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ComVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private void Change(string cmd)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";//指定启动命令窗口
+            p.StartInfo.UseShellExecute = false;//不使用系统外壳程序启动进程
+            p.StartInfo.CreateNoWindow = true;//不显示dos程序窗口
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;//把错误输出写到StandardError流中
+            p.Start();//开启进程
+            p.StandardInput.WriteLine(cmd + "&exit");//指定执行的命令
+            p.StandardInput.AutoFlush = true;//刷新缓冲区
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;//隐藏窗口样式
+            p.WaitForExit();//阻塞等待进程结束
+            p.Close();//关闭进程
+            p.Dispose();//释放资源
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp = Application.StartupPath + "\\Temp\\";//设置临时文件路径
+            string strMP4 = "";//记录最终mp4的路径
+            //转换临时片头
+            Change(Application.StartupPath + "\\ffmpeg -i " + textBox1.Text + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + temp + "begin.ts");
+            //转换临时片尾
+            Change(Application.StartupPath + "\\ffmpeg -i " + textBox3.Text + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + temp + "end.ts");
+            System.Threading.ThreadPool.QueueUserWorkItem(//使用线程池
+                 (P_temp) =>
+                 {
+                     button1.Enabled = false;
+                     for (int i = 0; i < listView1.Items.Count; i++)//遍历要合成的所有视频
+                     {
+                         //将遍历到的视频转换为临时视频文件
+                         Change(Application.StartupPath + "\\ffmpeg -i " + listView1.Items[i].Text + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + temp + "temp.ts");
+                         //合成临时文件
+                         Change("copy/b " + temp + "begin.ts + " + temp + "temp.ts + " + temp + "end.ts / y " + temp + "combine.ts");
+                         strMP4 = textBox4.Text.TrimEnd(new char[] { '\\' }) + "\\" + new FileInfo(listView1.Items[i].Text).Name;//记录最终mp4的路径
+                         if (File.Exists(strMP4))//判断文件是否已经存在
+                             File.Delete(strMP4);//如果存在则删除
+                         //生成最终的mp4
+                         Change(Application.StartupPath + "\\ffmpeg -i " + temp + "combine.ts -acodec copy -vcodec copy -absf aac_adtstoasc " + strMP4);
+                         File.Delete(temp + "temp.ts");//删除临时视频文件
+                         File.Delete(temp + "combine.ts");//删除临时合成文件
+                     }
+                     File.Delete(temp + "begin.ts");//删除临时片头文件
+                     File.Delete(temp + "end.ts");//删除临时片尾文件
+                     MessageBox.Show("视频合成成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                     button1.Enabled = true;
+                 });
+        }
+        //选择要转换的视频资源所在文件夹
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listView1.Items.Clear();//清空文件列表
+                textBox2.Text = folderBrowserDialog1.SelectedPath;//记录选择路径
+                DirectoryInfo dir = new DirectoryInfo(textBox2.Text);
+                FileSystemInfo[] files = dir.GetFiles();//获取文件夹中所有文件
+                string path = dir.FullName.TrimEnd(new char[] { '\\' }) + "\\";//获取文件所在目录
+                string newPath;
+                System.Threading.ThreadPool.QueueUserWorkItem(//使用线程池
+                     (P_temp) =>
+                     {
+                         foreach (FileInfo file in files)//遍历所有文件
+                         {
+                             newPath = file.FullName;
+                             if (file.Extension.ToLower() == ".mp4")//如果是视频文件
+                             {
+                                 if (file.Name.IndexOf(" ") != -1)
+                                 {
+                                     newPath = path + file.Name.Replace(" ", ""); ;//设置更名后的文件的完整路径
+                                     File.Copy(file.FullName, newPath, true);//将更名后的文件复制到原目录下
+                                     File.Delete(file.FullName);//删除原目录下的原始文件
+                                 }
+                                 listView1.Items.Add(newPath);//显示文件列表
+                             }
+                         }
+                     });
+            }
+        }
+        //设置片尾
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = openFileDialog1.FileName;//选择片尾
+            }
+        }
+        //设置片头
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;//选择片头
+            }
+        }
+        //设置视频保存路径
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //清空缓存文件
+            DirectoryInfo dinfo = new DirectoryInfo(Application.StartupPath + "\\Temp\\");
+            foreach (FileInfo f in dinfo.GetFiles())
+                f.Delete();
+        }
+    }
+}
